2a6ca82cf9172862115a50753310266dd4403f97
Aibek <a.sadraliev@gmail.com>
2022-04-26 20:43:09 +0600

    updated User entity - added phoneNuber, createdAt fields and specified types

--- src/modules/auth/entity/user.entity.ts
+++ src/modules/auth/entity/user.entity.ts
@@ -5,15 +5,21 @@ export default class UserEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ type: 'varchar', length: 50 })
   firstName: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 50 })
   lastName: string;
 
-  @Column()
+  @Column({ type: 'varchar', length: 50 })
   email: string;
+
+  @Column({ type: 'integer', width: 12, nullable: true })
+  phoneNumber: number;
 
   @Column({ select: false })
   password: string;
+
+  @Column({ type: 'timestamp' })
+  created_at: Date;
 }
