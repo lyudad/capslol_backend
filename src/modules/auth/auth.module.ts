3ea88821fe3f134925aa7d2d921b43150958d491
sapar6ek0v <razetka0309@gmail.com>
2022-05-01 09:09:24 +0300

    send a link

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MailModule } from 'src/mail/mail.module';
 import AuthController from './auth.contoller';
 import AuthServive from './auth.service';
 import UserEntity from './entity/user.entity';
+import PasswordController from './password.controller';
+import PasswordService from './password.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
-  controllers: [AuthController],
-  providers: [AuthServive],
+  imports: [TypeOrmModule.forFeature([UserEntity]), MailModule],
+  controllers: [AuthController, PasswordController],
+  providers: [AuthServive, PasswordService],
 })
 export default class AuthModule {}
