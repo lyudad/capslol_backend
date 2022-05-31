3450a27b89f2dbdb35a64181c8445df04ee49a31
Aibek Sadraliev <a.sadraliev@gmail.com>
2022-05-31 19:31:52 +0600

    new migration version

--- src/migrations/1654003845408-NewMigration.ts
+++ src/migrations/1654003845408-NewMigration.ts
@@ -1,17 +1,17 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class AddProjectDurationFieldIntoJobEntity1653643960997 implements MigrationInterface {
-    name = 'AddProjectDurationFieldIntoJobEntity1653643960997'
+export class NewMigration1654003845408 implements MigrationInterface {
+    name = 'NewMigration1654003845408'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`firstName\` varchar(50) NOT NULL, \`lastName\` varchar(50) NOT NULL DEFAULT '', \`role\` enum ('Freelancer', 'Job Owner', 'No set') NULL, \`email\` varchar(50) NOT NULL, \`phoneNumber\` varchar(25) NULL, \`password\` varchar(255) NULL, \`createdAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`isGoogle\` tinyint NULL DEFAULT 0, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`categories\` (\`id\` int NOT NULL AUTO_INCREMENT, \`categoryName\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`users\` (\`id\` int NOT NULL AUTO_INCREMENT, \`firstName\` varchar(50) NOT NULL, \`lastName\` varchar(50) NOT NULL DEFAULT '', \`role\` enum ('Freelancer', 'Job Owner', 'No set') NOT NULL DEFAULT 'No set', \`email\` varchar(50) NOT NULL, \`phoneNumber\` varchar(25) NULL, \`password\` varchar(255) NULL, \`createdAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`isGoogle\` tinyint NULL DEFAULT 0, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`educations\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, \`specialization\` varchar(255) NOT NULL, \`startAt\` varchar(255) NOT NULL, \`endAt\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`experiences\` (\`id\` int NOT NULL AUTO_INCREMENT, \`companyName\` varchar(255) NOT NULL, \`position\` varchar(255) NOT NULL, \`startAt\` varchar(255) NOT NULL, \`endAt\` varchar(255) NOT NULL, \`profileId\` int NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`profiles\` (\`id\` int NOT NULL AUTO_INCREMENT, \`profileImage\` varchar(385) NOT NULL, \`hourRate\` int NOT NULL, \`availableHours\` int NOT NULL, \`position\` varchar(255) NOT NULL, \`english\` enum ('No English', 'Beginner', 'Pre-Intermediate', 'Intermediate', 'Advanced', 'No set') NOT NULL DEFAULT 'No set', \`other\` text NOT NULL, \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updatedAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), \`userId\` int NULL, \`educationsId\` int NULL, \`experienseId\` int NULL, \`categoriesId\` int NULL, UNIQUE INDEX \`REL_315ecd98bd1a42dcf2ec4e2e98\` (\`userId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`skills\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(255) NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`jobs\` (\`id\` int NOT NULL AUTO_INCREMENT, \`title\` text NOT NULL, \`description\` text NOT NULL, \`price\` int NOT NULL, \`timeAvailable\` int NOT NULL, \`createdAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`languageLevel\` enum ('No English', 'Beginner', 'Pre-Intermediate', 'Intermediate', 'Advanced') NULL, \`projectDuration\` enum ('До 6 месяцев', '6 месяцев', 'Больше 6 месяцев') NULL, \`ownerId\` int NOT NULL, \`categoryId\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-        await queryRunner.query(`CREATE TABLE \`proposals\` (\`id\` int NOT NULL AUTO_INCREMENT, \`hourRate\` int NOT NULL, \`coverLetter\` varchar(255) NOT NULL, \`createdAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`jobId\` int NOT NULL, \`freelancerId\` int NOT NULL, UNIQUE INDEX \`REL_219edc10f7e4cc15dd19ea5d36\` (\`freelancerId\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
+        await queryRunner.query(`CREATE TABLE \`proposals\` (\`id\` int NOT NULL AUTO_INCREMENT, \`hourRate\` int NOT NULL, \`coverLetter\` varchar(255) NOT NULL, \`createdAt\` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, \`jobId\` int NOT NULL, \`freelancerId\` int NOT NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`skills_profiles\` (\`profilesId\` int NOT NULL, \`skillsId\` int NOT NULL, INDEX \`IDX_5c85effadd3d21e7132abbc222\` (\`profilesId\`), INDEX \`IDX_87f6c82e9f11876e6c4fefec3d\` (\`skillsId\`), PRIMARY KEY (\`profilesId\`, \`skillsId\`)) ENGINE=InnoDB`);
         await queryRunner.query(`CREATE TABLE \`skills_jobs\` (\`jobsId\` int NOT NULL, \`skillsId\` int NOT NULL, INDEX \`IDX_0c98d98cf0d76c90689d6e565d\` (\`jobsId\`), INDEX \`IDX_077f60d8e7f06770e4b39645e1\` (\`skillsId\`), PRIMARY KEY (\`jobsId\`, \`skillsId\`)) ENGINE=InnoDB`);
         await queryRunner.query(`ALTER TABLE \`experiences\` ADD CONSTRAINT \`FK_be01c61f0c549f2187b5c05c349\` FOREIGN KEY (\`profileId\`) REFERENCES \`profiles\`(\`id\`) ON DELETE SET NULL ON UPDATE NO ACTION`);
@@ -49,7 +49,6 @@ export class AddProjectDurationFieldIntoJobEntity1653643960997 implements Migrat
         await queryRunner.query(`DROP INDEX \`IDX_87f6c82e9f11876e6c4fefec3d\` ON \`skills_profiles\``);
         await queryRunner.query(`DROP INDEX \`IDX_5c85effadd3d21e7132abbc222\` ON \`skills_profiles\``);
         await queryRunner.query(`DROP TABLE \`skills_profiles\``);
-        await queryRunner.query(`DROP INDEX \`REL_219edc10f7e4cc15dd19ea5d36\` ON \`proposals\``);
         await queryRunner.query(`DROP TABLE \`proposals\``);
         await queryRunner.query(`DROP TABLE \`jobs\``);
         await queryRunner.query(`DROP TABLE \`skills\``);
@@ -57,8 +56,8 @@ export class AddProjectDurationFieldIntoJobEntity1653643960997 implements Migrat
         await queryRunner.query(`DROP TABLE \`profiles\``);
         await queryRunner.query(`DROP TABLE \`experiences\``);
         await queryRunner.query(`DROP TABLE \`educations\``);
-        await queryRunner.query(`DROP TABLE \`users\``);
         await queryRunner.query(`DROP TABLE \`categories\``);
+        await queryRunner.query(`DROP TABLE \`users\``);
     }
 
 }
