21ac0c18094e13f0e5cc9ee9b9ff5f011de0a833
sapar6ek0v <razetka0309@gmail.com>
2022-06-06 20:53:57 +0300

    add endpoints

---
+++ src/modules/message/message.controller.ts
@@ -0,0 +1,32 @@
+﻿import {
+  Controller,
+  UsePipes,
+  ValidationPipe,
+  HttpException,
+  HttpStatus,
+  Get,
+  Query,
+} from '@nestjs/common';
+import { ApiTags, ApiResponse } from '@nestjs/swagger';
+import MessageEntity from './entities/message.entity';
+import MessageService from './message.service';
+
+@ApiTags('Messages')
+@Controller('messages')
+export default class MessageController {
+  constructor(private readonly messageService: MessageService) {}
+
+  @ApiResponse({
+    type: MessageEntity,
+  })
+  @Get()
+  @UsePipes(new ValidationPipe())
+  async findByRoom(@Query('room') room: number): Promise<MessageEntity[]> {
+    try {
+      const payload = await this.messageService.findMessagesByRoomId(room);
+      return payload;
+    } catch (error) {
+      throw new HttpException(error.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+}
--- src/modules/message/message.module.ts
+++ src/modules/message/message.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import MessageService from './message.service';
 import MessageGateway from './message.gateway';
 import MessageEntity from './entities/message.entity';
+import MessageController from './message.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([MessageEntity])],
+  controllers: [MessageController],
   providers: [MessageGateway, MessageService],
 })
 export default class MessageModule {}
